fc6842d2984f57454ab8a01c1e86b97442661057
Monkkham <kkham2021@gmail.com>
2024-06-14 10:20:10 +0700

    khamdev

--- app/Http/Livewire/Backend/Reports/ReportUserContent.php
+++ app/Http/Livewire/Backend/Reports/ReportUserContent.php
@@ -3,20 +3,27 @@
 namespace App\Http\Livewire\Backend\Reports;
 
 use App\Models\Position;
+use App\Models\Role;
 use App\Models\User;
 use Livewire\Component;
 
 class ReportUserContent extends Component
 {
-    public $position_id;
+    public $roles_id,$gender,$status;
 
     public function render()
     {
         $data = User::get();
-        $position = Position::get();
-        if ($this->position_id) {
-            $data = $data->where('position_id', $this->position_id);
+        $roles = Role::get();
+        if ($this->roles_id) {
+            $data = $data->where('roles_id', $this->roles_id);
         }
-        return view('livewire.backend.reports.report-user-content', compact('data','position'))->layout('layouts.backend.style');
+        if ($this->gender) {
+            $data = $data->where('gender', $this->gender);
+        }
+        if ($this->status) {
+            $data = $data->where('status', $this->status);
+        }
+        return view('livewire.backend.reports.report-user-content', compact('data','roles'))->layout('layouts.backend.style');
     }
 }
--- app/Http/Livewire/Backend/Reports/ReportWorkPlaceContent.php
+++ app/Http/Livewire/Backend/Reports/ReportWorkPlaceContent.php
@@ -2,21 +2,39 @@
 
 namespace App\Http\Livewire\Backend\Reports;
 
-use Livewire\Component;
-use App\Models\Position;
+use App\Models\District;
+use App\Models\Province;
+use App\Models\Village;
 use App\Models\WorkPlace;
+use Livewire\Component;
 
 class ReportWorkPlaceContent extends Component
 {
-    public $position_id,$end_date,$start_date;
-
+    public $position_id, $end_date, $start_date, $province_id, $district_id, $village_id;
+    public $districts = [],
+    $villages = [];
     public function render()
     {
-        $data = WorkPlace::get();
+        $data = WorkPlace::query();
+        $province = Province::all();
         $end = date('Y-m-d H:i:s', strtotime($this->end_date . '23:23:59'));
         if ($this->start_date && $this->end_date) {
             $data = $data->whereBetween('created_at', [$this->start_date, $end]);
         }
-        return view('livewire.backend.reports.report-work-place-content',compact('data'))->layout('layouts.backend.style');
+        if ($this->province_id) {
+            $this->districts = District::where('province_id', $this->province_id)->get();
+            $data->where('province_id', $this->province_id);
+        }
+
+        if ($this->district_id) {
+            $this->villages = Village::where('district_id', $this->district_id)->get();
+            $data->where('district_id', $this->district_id);
+        }
+
+        if ($this->village_id) {
+            $data->where('village_id', $this->village_id);
+        }
+        $data = $data->get();
+        return view('livewire.backend.reports.report-work-place-content', compact('data', 'province'))->layout('layouts.backend.style');
     }
 }
